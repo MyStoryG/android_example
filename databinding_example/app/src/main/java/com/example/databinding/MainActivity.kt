ee483fbda491b60b0def95528de47e95ce010b6f
MyStoryG <seo8703@gmail.com>
2021-02-14 07:51:03 +0900

    databinding example

---
+++ databinding_example/app/src/main/java/com/example/databinding/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.databinding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.databinding.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    var text = "Hello World!"
+    var isClicked = false
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.test = this
+
+        binding.hideButton.setOnClickListener {
+            isClicked = !isClicked
+            binding.invalidateAll()
+        }
+    }
+}
